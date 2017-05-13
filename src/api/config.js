abbda0bbca6f9b28144a792f0b2c87fd1a2b6bf5
bere <angelnefsy_bell@hotmail.com>
2017-05-12 20:36:09 -0500

    First commit

---
+++ src/api/config.js
@@ -0,0 +1,22 @@
+
+/*
+
+API account created
+Here are the details of your new API account.
+Application name	music
+API key	8a572b76bd8efbb3852241cf03514fbf
+Shared secret	88cdbd4efba95456cfb221eed1dc9c31
+Registered to	Berenu1
+
+
+*/
+
+export default {
+appname:  'music',
+apiKey: '8a572b76bd8efbb3852241cf03514fbf',
+secret: '88cdbd4efba95456cfb221eed1dc9c31',
+registeredTo: 'Berenu13',
+
+
+
+}
